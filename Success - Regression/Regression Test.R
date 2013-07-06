b1251d08497784bb5a38c990c06433a32f130563
upjohnc <upjohnc@gmail.com>
2013-07-06 07:56:21 -0500

    Started Building dataframes

--- Success - Regression/Regression Test.R
+++ Success - Regression/Regression Test.R
@@ -1,5 +1,19 @@
 
 setwd('/Users/upjohnc/Golf Statistics')
-all.players<-read.csv('./data/PGA Stats.csv')
+all<-read.csv('./data/PGA Stats.csv', header=FALSE, stringsAsFactors=F)
 
-all.players[1,]
+
+header <- all[c(8,13,18,23,33,38,104, 56, 61), 1]
+header<-append(c("Name", "Year"), header)
+header
+temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 1]
+temp2<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 2]
+temp3<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 3]
+temp <- rbind(temp1, temp2, temp3)
+temp
+class(all[9,1])
+
+rbind(header, temp)
+
+all[c(1,2,8,13,18,23,33,38,104, 56, 61), 1]
+all[1,]
