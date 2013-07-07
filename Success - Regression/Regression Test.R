4640f619851adc96960aca4ced806f5321ec03d7
upjohnc <upjohnc@gmail.com>
2013-07-06 20:40:07 -0500

    more fixes

--- Success - Regression/Regression Test.R
+++ Success - Regression/Regression Test.R
@@ -1,7 +1,6 @@
 
 setwd('/Users/upjohnc/Golf Statistics')
 all<-read.csv('./data/PGA Stats.csv', header=FALSE, stringsAsFactors=F)
-
 header <- all[c(8,13,18,23,33,38,104, 56, 61), 1]
 header<-append(c("Name", "Year"), header)
 
@@ -11,7 +10,7 @@ temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 2]
 temp3<- all[c(1,2,9,14,19,24,34,39,106, 53, 63), 3]
 temp <- rbind(temp1, temp2, temp3)
 tiger<-rbind(header, temp)
-tiger<-as.data.frame(temp)
+tiger<-as.data.frame(temp, stringsAsFactors=F)
 colnames(tiger) <- header
 ####Crane####
 temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 4]
@@ -19,7 +18,7 @@ temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 5]
 temp3<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 6]
 temp <- rbind(temp1, temp2, temp3)
 crane<-rbind(header, temp)
-crane<-as.data.frame(temp)
+crane<-as.data.frame(temp, stringsAsFactors=F)
 colnames(crane) <- header
 ####Baddeley####
 temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 7]
@@ -27,7 +26,7 @@ temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 8]
 temp3<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 9]
 temp <- rbind(temp1, temp2, temp3)
 baddeley<-rbind(header, temp)
-baddeley<-as.data.frame(temp)
+baddeley<-as.data.frame(temp, stringsAsFactors=F)
 colnames(baddeley) <- header
 ####Watson####
 temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 10]
@@ -35,7 +34,7 @@ temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 11]
 temp3<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 12]
 temp <- rbind(temp1, temp2, temp3)
 watson<-rbind(header, temp)
-watson<-as.data.frame(temp)
+watson<-as.data.frame(temp, stringsAsFactors=F)
 colnames(watson) <- header
 ####Kuchar####
 temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 13]
@@ -43,7 +42,7 @@ temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 14]
 temp3<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 15]
 temp <- rbind(temp1, temp2, temp3)
 kuchar<-rbind(header, temp)
-kuchar<-as.data.frame(temp)
+kuchar<-as.data.frame(temp, stringsAsFactors=F)
 colnames(kuchar) <- header
 ####Bradeley####
 temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 16]
@@ -51,7 +50,7 @@ temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 17]
 temp3<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 18]
 temp <- rbind(temp1, temp2, temp3)
 bradeley<-rbind(header, temp)
-bradeley<-as.data.frame(temp)
+bradeley<-as.data.frame(temp, stringsAsFactors=F)
 colnames(bradeley) <- header
 ####Duval####
 temp1<- all[c(1,2,9,14,19,24,34,39,91, 57, 62), 19]
@@ -59,15 +58,21 @@ temp2<- all[c(1,2,9,14,19,24,34,39,96, 48, 53), 20]
 temp3<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 21]
 temp <- rbind(temp1, temp2, temp3)
 duval<-rbind(header, temp)
-duval<-as.data.frame(temp)
+duval<-as.data.frame(temp, stringsAsFactors=F)
 colnames(duval) <- header
+lapply(duval, class)
+#set $0 for his 2013 season
+duval[,10:11] <- as.numeric(as.character(duval[,10:11]))
+lapply(duval, class)
+duval[1,10:11] <- 0
+duval[,10:11]
 ####Fowler####
 temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 22]
 temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 23]
 temp3<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 24]
 temp <- rbind(temp1, temp2, temp3)
 fowler<-rbind(header, temp)
-fowler<-as.data.frame(temp)
+fowler<-as.data.frame(temp, stringsAsFactors=F)
 colnames(fowler) <- header
 ####Furyk####
 temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 25]
@@ -75,7 +80,7 @@ temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 26]
 temp3<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 27]
 temp <- rbind(temp1, temp2, temp3)
 furyk<-rbind(header, temp)
-furyk<-as.data.frame(temp)
+furyk<-as.data.frame(temp, stringsAsFactors=F)
 colnames(furyk) <- header
 ####Clark####
 temp1<- all[c(1,2,9,14,19,24,34,39,105, 57, 62), 28]
@@ -83,6 +88,6 @@ temp2<- all[c(1,2,9,14,19,24,34,39,110, 57, 67), 29]
 temp3<- all[c(1,2,9,14,19,24,34,39,101, 48, 58), 30]
 temp <- rbind(temp1, temp2, temp3)
 clark<-rbind(header, temp)
-clark<-as.data.frame(temp)
+clark<-as.data.frame(temp, stringsAsFactors=F)
 colnames(clark) <- header
 rm(temp, temp1, temp2, temp3, header)
